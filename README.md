0114f91906cf95b11b8c9c0302d4481d421592a7
michael <quocanh261997@gmail.com>
2017-12-03 10:38:11 -0500

    Second commit

---
+++ README.md
@@ -0,0 +1,43 @@
+#### Name: Quoc Anh Nguyen, Andre Le
+#### Class: CSE 283 - Section A
+#### Date: 12/02/2017
+#### Assignment: Project 2: Web Proxy
+
+## Instructions running the program
+
+1. Download the zip file and extract it
+2. Make sure you have installed Python 2 on your computer
+3. This step is different for the different type of Operating System your are using:
+
+ * If you use Windows, go to **Control Panel** => **Internet Options** => **Connections** => **LAN Settings** => **Proxy Server**. Once in it, check the box for **Proxy Server where Use a proxy server for your LAN**, and enter the *Address* and *Port* that you would use for the proxy. To fine-tune the proxy settings such that they are only applied to HTTP (and not HTTPS, FTP, etc.) you can use the **Advanced** push button.In this case, the address should be **localhost** and port number will be of your choice.
+
+ * If you use Ubuntu, go to Unity Dash and type **"Network"**, and choose the **Network Proxy** option. Change the method from **Automatic** to **Manual** and enter the address and port for HTTP Proxy only. In this case, the address should be **localhost** and port number will be of your choice. Once done, click **Apply system wide** to enable it.
+
+ * If you use MacOS, choose **System Preferences** and click **Network**. Choose **Advanced** and then choose **Proxies**. Configure the address and the port number there and apply it. In this case, the address should be **localhost** and port number will be of your choice.
+
+4. Open your terminal and navigate to the extracted folder. Type in this command:
+```
+python proxy.py <yourPortNumber>
+```
+with *yourPortNumber* as the port you would like the proxy to serve with. This port number **must** match the port number that you enter in the proxy settings in the last step.
+
+5. The proxy should send in a message in the terminal saying "Ready to serve, listening to port ...."
+
+6. Open any browser, and type in the website name you would like to see.
+
+7. If the websites you enter has contents or urls matched one of the bad keywords, it will be redirected to one of the error website. **Note: This only applies to website serves over HTTP**
+
+8. Enter **Ctrl+C** to terminate the proxy. Remember to change back the proxy settings to resume your normal network activities.
+
+## Manual
+* The function **serve_connection** implements feature 2.
+* The function **contains_keywords** and **serve_connection** implements feature 3.
+* Line **17** supports user choice of port number
+* The function **check_for_content** checks for which content needed to be check and implements feature 8
+
+## Functionality
+* The proxy can get you to your basic webpages
+* It will block that webpage if it contains bad keywords in the url or content, and redirect you back to the error webpage. Note this only applies to HTTP webpage. Other HTTPS won't be blocked
+
+## Testing
+* The code can go through every case that the project requires, except the bad content file, since it cannot decode the data and cannot do a check for bad keywords to block it.
--- proxy.py
+++ proxy.py
@@ -1,10 +1,20 @@
+"""
+Name: Quoc Anh Nguyen, Andre Le
+Class: CSE 283 - Section A
+Date: 12/02/2017
+Assignment: Project 2 - Net Nonny: A Web Proxy Based Service
+"""
+
+#Import necessary module for the program
 import socket
 import sys
 import thread
+import gzip
+import struct
 
 HTTP_PORT = 80
 HOST = ""
-port_num = 2606
+port_num = int(sys.argv[1])
 MAX_CONNECTIONS = 1000
 BUFFER_SIZE = 16384
 BAD_URL_HOST = "http://ceclnx01.eas.miamioh.edu/~gomezlin/error.html"
@@ -12,7 +22,9 @@ BAD_CONTENT_HOST = "http://ceclnx01.eas.miamioh.edu/~gomezlin/error2.html"
 SUFFIXES = [".png", ".jpg", ".jpeg", ".js", ".cs", ".gif"]
 BAD_KEYWORD = ["spongebob","britney spears","paris hilton"]
 
+#The main function that runs everything
 def main():
+    #Socket configuration
     try:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
@@ -22,11 +34,13 @@ def main():
     except socket.error, message:
         print "Failed to connect, error code: ", str(message[0]), ", Error message: ", str(message[1])
         sys.exit(1)
+    #Start the thread
     while 1:
         connection, address = server.accept()
         thread.start_new_thread(serve_connection, (connection, address))
     server.close()
 
+#Parse first line of request to get the url
 def get_url(first_line):
     url = ""
     try:
@@ -36,6 +50,8 @@ def get_url(first_line):
     finally:
         return url
 
+#Parse the url to a web server
+#Ex: "http://www.google.com/index.html" => "www.google.com"
 def url_to_web_server(url):
     http_pos = url.find("://")
     if http_pos == -1:
@@ -47,37 +63,46 @@ def url_to_web_server(url):
         web_server_pos = len(temp)
     return temp[:web_server_pos].split(":")[0]
 
+#Check if file content suffixes that isn't going to be inspected
 def check_for_content(url):
     if any (url.endswith(suffix) for suffix in SUFFIXES):
         return False
     return True
 
+#Return if the line contains any keyword
 def contains_keywords(line, keywords):
+    keywords.append("badtest")
     for seq in keywords:
         if len(seq.split(" "))>1:
             if all(s in line.lower() for s in seq.split(" ")):
                 return True
     if any (s in line.lower() for s in keywords):
         return True
+    keywords.remove("badtest")
     return False
 
+#Print the info the request to the console
 def print_info(type, request, address):
     print address[0], "\t", type.upper(), "\t", request
 
+#Redirect the user back to a webpage
 def redirect_response(url):
     return "HTTP/1.1 302 Found\r\nLocation: " + url + "\r\nHost: " + url_to_web_server(url) + "\r\nConnection: close\r\n\r\n"
 
+#Establish the connection
 def serve_connection(connection, address):
+    #Receive a request from the user
     data = connection.recv(BUFFER_SIZE)
     first_line = data.split("\n")[0]
     url = get_url(first_line)
     web_server = url_to_web_server(url)
     badUrl = False
 
+    #Serve "GET" request only
     if "GET" in first_line:
         print_info("request", first_line, address)
         badUrl = contains_keywords(url, BAD_KEYWORD)
-        print badUrl
+        #Check if the url contains block keywords
         if badUrl:
             print_info("blacklisted", url, address)
 
@@ -85,6 +110,8 @@ def serve_connection(connection, address):
     try:
         served_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         served_socket.connect((web_server,HTTP_PORT))
+        #If the url contains bad keyword(s), shut down the connection and
+        #redirect the users to the error webpage
         if badUrl:
             served_socket.shutdown(socket.SHUT_RDWR)
             connection.shutdown(socket.SHUT_RD)
@@ -101,6 +128,7 @@ def serve_connection(connection, address):
                             if badContent:
                                 print_info("bad content",url, address)
                                 break
+                    #If the content contains bad keywords, redirect the users
                     if badContent:
                         served_socket.shutdown(socket.SHUT_RDWR)
                         connection.shutdown(socket.SHUT_RD)
@@ -117,10 +145,11 @@ def serve_connection(connection, address):
         print "\t> Connection closed. Thread exiting..."
         thread.exit()
 
+#Run the main function
 if __name__ == "__main__":
     try:
         print "Forbidden keywords:", BAD_KEYWORD
         main()
     except KeyboardInterrupt:
-        print " ---> Caught SIGINT. Stopping server..."
+        print "Stopping server..."
         sys.exit(1)
